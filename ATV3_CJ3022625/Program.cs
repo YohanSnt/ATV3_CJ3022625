f6ac6b06915a551829f03698ad1fa6624c4e80cb
YohanSnt <yohan.s@aluno.ifsp.edu.br>
2024-03-06 08:59:34 -0300

    Adicionar arquivos de projeto.

---
+++ ATV3_CJ3022625/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Design;
+
+namespace ATV3_CJ3022625
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float n;
+
+            Console.WriteLine("Digite a nota média do aluno: ");
+            n = float.Parse(Console.ReadLine());
+
+            if (n >= 6)
+            {
+                Console.WriteLine("Aluno aprovado");
+
+
+            }
+            else Console.WriteLine("Aluno reprovado");
+        }
+    }
+}
